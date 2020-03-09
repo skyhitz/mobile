d8605dbc628c249e9695fc92c1c39e7797ae83d1
Brendan Sheehan <brendan@adp.bz>
2020-03-09 18:43:15 -0500

    add typings for window

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -8,6 +8,18 @@ import { initializeGoogleTagManager } from 'app/modules/marketing/web/GoogleTagM
 class CustomDocument extends Document {
   componentDidMount() {
     initializeGoogleTagManager();
+    if ('serviceWorker' in navigator) {
+      window.addEventListener('load', () => {
+        navigator.serviceWorker
+          .register('/service-worker.js', { scope: '/' })
+          .then(registration => {
+            console.log('SW registered: ', registration);
+          })
+          .catch(registrationError => {
+            console.log('SW registration failed: ', registrationError);
+          });
+      });
+    }
   }
   render() {
     return (
