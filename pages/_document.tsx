bc5bd050abef4542d872c52317817a4b123add3f
Brendan Sheehan <brendan@adp.bz>
2020-03-10 11:03:10 -0500

    add async to gtm script

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -11,6 +11,7 @@ class CustomDocument extends Document {
       <Html lang="en">
         <Head>
           <script
+            async
             dangerouslySetInnerHTML={{
               __html: `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
               new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
