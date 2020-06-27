ea2bde4254b0bf8c7575320cd105bdb19b755bdf
Brendan Sheehan <brendan@adp.bz>
2020-06-27 18:23:03 -0500

    use focus

--- modules/search/SearchEntryView.tsx
+++ modules/search/SearchEntryView.tsx
@@ -1,19 +1,18 @@
-import React, { useEffect } from 'react';
+import React, { useCallback } from 'react';
 import { observer } from 'mobx-react';
 import SearchEntryList from 'app/modules/search/SearchEntryList';
 import RecentlyAdded from 'app/modules/search/RecentlyAdded';
-import { useIsFocused } from '@react-navigation/native';
+import { useFocusEffect } from '@react-navigation/native';
 import { Stores } from 'app/functions/Stores';
 
 export default observer(() => {
-  const isFocused = useIsFocused();
   let { entriesSearchStore, inputSearchStore } = Stores();
 
-  useEffect(() => {
-    if (isFocused) {
+  useFocusEffect(
+    useCallback(() => {
       inputSearchStore.updateSearchType('entries');
-    }
-  }, []);
+    }, [])
+  );
 
   if (entriesSearchStore.active) {
     return <SearchEntryList />;
--- modules/search/SearchUserView.tsx
+++ modules/search/SearchUserView.tsx
@@ -1,19 +1,18 @@
-import React, { useEffect } from 'react';
+import React, { useCallback } from 'react';
 import { observer } from 'mobx-react';
 import SearchUserList from 'app/modules/search/SearchUserList';
 import TopRecentUserView from 'app/modules/search/TopRecentUserView';
-import { useIsFocused } from '@react-navigation/native';
+import { useFocusEffect } from '@react-navigation/native';
 import { Stores } from 'app/functions/Stores';
 
 export default observer(() => {
-  const isFocused = useIsFocused();
   let { usersSearchStore, inputSearchStore } = Stores();
 
-  useEffect(() => {
-    if (isFocused) {
+  useFocusEffect(
+    useCallback(() => {
       inputSearchStore.updateSearchType('users');
-    }
-  }, []);
+    }, [])
+  );
 
   if (usersSearchStore.active) {
     return <SearchUserList />;
