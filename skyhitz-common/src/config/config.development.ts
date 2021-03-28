0a559697b683b677cd9019a4fa5f6d198537b79b
Alejo Mendoza <alejandro@skyhitzmusic.com>
2021-03-28 16:29:06 -0500

    ignore build step

--- skyhitz-common/src/config/config.development.ts
+++ skyhitz-common/src/config/config.development.ts
@@ -1,7 +1,7 @@
 import { ConfigInterface } from './config';
 
 export const DevelopmentConfig: ConfigInterface = {
-  GRAPHQL_URL: 'http://192.168.43.24:4000/graphql',
+  GRAPHQL_URL: 'http://localhost:4000/api/graphql',
   ALGOLIA_APP_ID: 'HSVI9OH0KZ',
   ALGOLIA_SEARCH_KEY: '945b2ada1a0d291c0ee8c278d60fb495',
   STRIPE_PUBLISHABLE_KEY: 'pk_test_jiDVP0p6iRtILtE4FzFfMACc',
