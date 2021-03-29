9dab8927391f7e579b9e7ef30e8aa2c6615496ee
Alejo Mendoza <alejandro@skyhitzmusic.com>
2021-03-29 16:00:20 -0500

    update schema

--- skyhitz-common/src/config/config.development.ts
+++ skyhitz-common/src/config/config.development.ts
@@ -5,5 +5,5 @@ export const DevelopmentConfig: ConfigInterface = {
   ALGOLIA_APP_ID: 'HSVI9OH0KZ',
   ALGOLIA_SEARCH_KEY: '945b2ada1a0d291c0ee8c278d60fb495',
   STRIPE_PUBLISHABLE_KEY: 'pk_test_jiDVP0p6iRtILtE4FzFfMACc',
-  SCHEMA: 'exp://localhost:19000',
+  SCHEMA: 'exp://192.168.103.24:19000/--/',
 };
--- skyhitz-common/src/config/config.staging.ts
+++ skyhitz-common/src/config/config.staging.ts
@@ -6,5 +6,5 @@ export const StagingConfig: ConfigInterface = {
   ALGOLIA_APP_ID: 'HsSVI9OH0KZ',
   ALGOLIA_SEARCH_KEY: '945b2ada1a0d291c0ee8c278d60fb495',
   STRIPE_PUBLISHABLE_KEY: 'pk_test_jiDVP0p6iRtILtE4FzFfMACc',
-  SCHEMA: 'exp://192.168.103.24:19000/',
+  SCHEMA: 'exp://192.168.103.24:19000/--/',
 };
