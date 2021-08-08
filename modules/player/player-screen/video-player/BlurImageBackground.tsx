eec38ac926c42eb69b1376c28576a72b8cdcde32
Alejo Mendoza <alejandro@skyhitzmusic.com>
2021-08-08 08:55:58 -0500

    add blur to player

---
+++ modules/player/player-screen/video-player/BlurImageBackground.tsx
@@ -0,0 +1,19 @@
+import { ImageBackground } from 'react-native';
+import { BlurView } from 'expo-blur';
+
+function BlurImageBackground(props) {
+  return (
+    <ImageBackground
+      source={{
+        uri: props.imageUrl,
+      }}
+      style={{
+        opacity: props.opacity,
+      }}
+    >
+      <BlurView intensity={props.intensity}>{props.children}</BlurView>
+    </ImageBackground>
+  );
+}
+
+export default BlurImageBackground;
--- modules/player/player-screen/video-player/VideoComponent.tsx
+++ modules/player/player-screen/video-player/VideoComponent.tsx
@@ -4,50 +4,61 @@ import { Stores } from 'app/functions/Stores';
 import { Platform, StyleSheet } from 'react-native';
 import { videoWidth } from './VideoConstants';
 import { useState } from 'react';
+import BlurImageBackground from './BlurImageBackground';
 
 export default observer(({ dynamicHeight, desktop = false }) => {
   let { playerStore } = Stores();
   const [dynamicWidth, setDynamicWidth] = useState<number>(dynamicHeight);
   const [dynamicOpacity, setDynamicOpacity] = useState<number>(0);
+  const [blur, setBlur] = useState<number>(0);
 
   return (
-    <Video
-      posterSource={{ uri: playerStore.entry?.imageUrl }}
-      usePoster={true}
-      source={{
-        uri: playerStore.streamUrl,
-      }}
-      posterStyle={styles.poster}
-      ref={(ref) => playerStore.mountVideo(ref)}
-      onPlaybackStatusUpdate={(status) =>
-        playerStore.onPlaybackStatusUpdate(status)
-      }
-      resizeMode={Video.RESIZE_MODE_CONTAIN}
-      style={[
-        { opacity: dynamicOpacity },
-        desktop
-          ? {
-              maxWidth: dynamicWidth,
-              width: dynamicWidth,
-              minWidth: dynamicWidth,
-            }
-          : { height: dynamicHeight, maxHeight: 360 },
-        desktop ? styles.videoPlayerDesktop : styles.videoPlayer,
-      ]}
-      onError={(error) => playerStore.onError(error)}
-      onFullscreenUpdate={(update) => playerStore.onFullscreenUpdate(update)}
-      onReadyForDisplay={(res: any) => {
-        if (Platform.OS !== 'web') return;
-        const { videoHeight, videoWidth } = res.target;
-        const aspectRatio = videoWidth / videoHeight;
-        if (videoWidth === 0) {
-          return;
+    <BlurImageBackground
+      imageUrl={playerStore.entry?.imageUrl}
+      opacity={dynamicOpacity}
+      intensity={blur}
+    >
+      <Video
+        posterSource={{ uri: playerStore.entry?.imageUrl }}
+        usePoster={true}
+        source={{
+          uri: playerStore.streamUrl,
+        }}
+        posterStyle={[styles.poster, { width: dynamicWidth }]}
+        ref={(ref) => playerStore.mountVideo(ref)}
+        onPlaybackStatusUpdate={(status) =>
+          playerStore.onPlaybackStatusUpdate(status)
         }
-        setDynamicWidth(Math.ceil(aspectRatio * dynamicHeight));
-        // add delay or transition
-        setDynamicOpacity(1);
-      }}
-    />
+        resizeMode={Video.RESIZE_MODE_CONTAIN}
+        style={[
+          { opacity: dynamicOpacity },
+          desktop
+            ? {
+                maxWidth: dynamicWidth,
+                width: dynamicWidth,
+                minWidth: dynamicWidth,
+              }
+            : { height: dynamicHeight, maxHeight: 360 },
+          desktop ? styles.videoPlayerDesktop : styles.videoPlayer,
+        ]}
+        onError={(error) => playerStore.onError(error)}
+        onFullscreenUpdate={(update) => playerStore.onFullscreenUpdate(update)}
+        onReadyForDisplay={(res: any) => {
+          if (Platform.OS !== 'web') return;
+          const { videoHeight, videoWidth } = res.target;
+          const aspectRatio = videoWidth / videoHeight;
+          if (videoWidth === 0) {
+            setDynamicOpacity(1);
+            return;
+          }
+          setDynamicWidth(Math.ceil(aspectRatio * dynamicHeight));
+          // add delay or transition
+
+          setBlur(80);
+          setDynamicOpacity(1);
+        }}
+      />
+    </BlurImageBackground>
   );
 });
 
