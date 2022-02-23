a01b063263f1ddc45980ca9287db3161e56acc48
Alejo Mendoza <alejandro@skyhitzmusic.com>
2022-02-23 13:25:31 -0500

    upgrade

--- modules/ui/EntryPrice.tsx
+++ modules/ui/EntryPrice.tsx
@@ -3,6 +3,15 @@ import { StyleSheet, View, Text } from 'react-native';
 import DollarIcon from 'app/modules/ui/icons/dollar';
 import Colors from 'app/constants/Colors';
 import { Stores } from 'app/functions/Stores';
+import { A } from '@expo/html-elements';
+import { Config } from 'app/skyhitz-common/src/config';
+
+const stellarExpertLink = (code: string, issuer: string) =>
+  `https://stellar.expert/explorer/${
+    Config.HORIZON_URL === "'https://horizon-testnet.stellar.org'"
+      ? 'testnet'
+      : 'pubnet'
+  }/asset/${code}-${issuer}`;
 
 function EntryPrice({ code, issuer }) {
   const [value, setValue] = useState({ price: 0, amount: 0 });
@@ -21,7 +30,13 @@ function EntryPrice({ code, issuer }) {
   return (
     <View style={styles.endSection}>
       {value.price > 0 && <Text style={styles.price}>{value.price}</Text>}
-      <DollarIcon size={12} color={Colors.white} />
+      <A
+        target="_blank"
+        href={stellarExpertLink(code, issuer)}
+        aria-label="View on Stellar Expert"
+      >
+        <DollarIcon size={12} color={Colors.white} />
+      </A>
     </View>
   );
 }
