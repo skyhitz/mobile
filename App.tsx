dcca7972c33d0057ff415421577333b74a7ab341
Alejo Mendoza <alejandro@skyhitzmusic.com>
2022-02-28 09:51:42 -0500

    log top level error

--- App.tsx
+++ App.tsx
@@ -3,6 +3,12 @@ import React, { lazy, Suspense } from 'react';
 import RootNavigation from 'app/modules/navigation/RootNavigation';
 import '@expo/match-media';
 import useCachedResources from './functions/CacheResourcesAsync';
+import ErrorBoundary from 'react-native-error-boundary';
+
+const errorHandler = (error: Error, stackTrace: string) => {
+  console.log('error', error);
+  /* Log the error to an error reporting service */
+};
 const Providers = lazy(() => import('app/modules/providers/Providers'));
 
 import LoadingScreen from 'app/modules/accounts/LoadingScreen';
@@ -23,8 +29,10 @@ export default () => {
     return <LoadingScreen />;
   }
   return (
-    <ProvidersSuspense>
-      <RootNavigation />
-    </ProvidersSuspense>
+    <ErrorBoundary onError={errorHandler}>
+      <ProvidersSuspense>
+        <RootNavigation />
+      </ProvidersSuspense>
+    </ErrorBoundary>
   );
 };
